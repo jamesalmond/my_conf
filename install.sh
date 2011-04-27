fad58a0d7a3530689ba8d1f056df2114838ef300
JamesAlmond <james@jamesalmond.com>
2011-04-27 16:35:05 +0100

    Update install script to use github gitconfig

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ ln -s ~/.my_conf/zshrc ~/.zshrc
 ln -s ~/.my_conf/gvimrc_local ~/.gvimrc.local
 ln -s ~/.my_conf/vimrc_local ~/.vimrc.local
 ln -s ~/.my_conf/janus_rake ~/.janus.rake
-ln -s ~/.my_conf/gitconfig ~/.gitconfig
+ln -s ~/.my_conf/gitconfig_with_token ~/.gitconfig
 ln -s ~/.my_conf/gemrc ~/.gemrc
 ln -s ~/.my_conf/rvmrc ~/.rvmrc
