d4a9c84b263b3bce9c0de896ed372abb310a8574
JamesAlmond <james@jamesalmond.com>
2011-04-27 15:41:16 +0100

    Added gemrc to config

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s ~/.my_conf/gvimrc_local ~/.gvimrc.local
 ln -s ~/.my_conf/vimrc_local ~/.vimrc.local
 ln -s ~/.my_conf/janus_rake ~/.janus.rake
 ln -s ~/.my_conf/gitconfig ~/.gitconfig
+ln -s ~/.my_conf/gemrc ~/.gemrc
