b1a0c2f1ed8c7f2a254535cb620b360279ef9f28
James Almond <jamesalmond@James-iMac.local>
2011-04-27 15:34:09 +0100

    Fixed zsh in install file

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-ln -s zshrc ~/.zshrc
-ln -s gvimrc_local ~/.gvimrc.local
-ln -s vimrc_local ~/.vimrc.local
-ln -s janus_rake ~/.janus.rake
+ln -s ~/.my_conf/zshrc ~/.zshrc
+ln -s ~/.my_conf/gvimrc_local ~/.gvimrc.local
+ln -s ~/.my_conf/vimrc_local ~/.vimrc.local
+ln -s ~/.my_conf/janus_rake ~/.janus.rake
+ln -s ~/.my_conf/gitconfig ~/.gitconfig
 
